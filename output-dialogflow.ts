d28b680474476a266f6315fb4ed2ffa6153e6c8a
Peter Nann <peter.nann@gmail.com>
2018-03-22 14:19:47 +0000

    Support Dialogflow V2 API Intent/Entity formats. Importing untested.

--- output-dialogflow.ts
+++ output-dialogflow.ts
@@ -17,7 +17,7 @@ interface DialogflowEntityItem {
 };
 
 /** Structure used for one example sentence in Dialogflow 'intents/*_usersays_en.json' file */
-interface DialogflowUserSaysItem {
+interface DialogflowUserSaysItem_V1 {
     id: string,
     data: Array<{ text: string, userDefined: boolean }>,
     isTemplate: boolean,
@@ -25,19 +25,22 @@ interface DialogflowUserSaysItem {
     updated: number
 };
 
+interface DialogflowUserSaysItem {
+    name: string,
+    type: string,
+    timesAddedCount: number,
+    parts: any[]
+};
+
 
 export function outputDialogflow(outputFolder: string) {
     LOG("outputDialogflow()...");
-    let fd: number, outfilePath: string;
+    let fd: number, outfilePath: string, guid: string;
 
     //checkAgentRootFiles(outputFolder);
 
     let data = global.dieData;
 
-    // const intentsPath = `${outputFolder}/intents`;
-    // const entitiesPath = `${outputFolder}/entities`;
-    // if (data.intents.length > 0) mkdir(intentsPath);
-    // if (data.entities.length > 0) mkdir(entitiesPath);
     const lang = data.lang.substr(0, 2);
     let comma: string;   // - Array separator mechanism
 
@@ -62,38 +65,141 @@ export function outputDialogflow(outputFolder: string) {
     //     }
     // }
 
-    // For each Entity, write out the 'entries' file:
-    for (let sourceEntity of data.entities) {
-        outfilePath = `${outputFolder}/ENTITY-${sourceEntity.name}.json`;
+    // For each Intent, write out the 'usersays' file:
+    for (let sourceIntent of data.intents)
+        writeIntent(sourceIntent, outputFolder);
 
-        try {
-            fd = fs.openSync(outfilePath, 'w');
-            fs.appendFileSync(fd,
-                `{\n` +
-                `"name": "",\n` +
-                `"displayName": "Animal",\n` +
-                `"kind": "KIND_MAP",\n` +
-                `"autoExpansionMode": "AUTO_EXPANSION_MODE_UNSPECIFIED",\n` +
-                `"entities": [`
-            );
-            console.log(`Writing to ${outfilePath}`);
-            let entityItem: DialogflowEntityItem;
-            comma = "";   // - Array separator mechanism
-            for (let sentence of sourceEntity.expandedSentences) {
-                entityItem = { value: sentence, synonyms: [sentence] };
-                fs.appendFileSync(fd, comma + '\n' + JSON.stringify(entityItem, null, 2));
-                if (comma === "") comma = ',';
-            }
-            fs.appendFileSync(fd, '\n]\n}');
-            fs.closeSync(fd);
-        } catch (err) {
-            ERROR(`ERROR: ${err}\nWriting Dialogflow entity file: ${outfilePath}`);
-            process.exit(1);
+
+    // For each Entity, write out the 'entries' file:
+    for (let sourceEntity of data.entities)
+        writeEntity(sourceEntity, outputFolder);
+
+}
+
+function writeIntent(sourceIntent, outputFolder: string) {
+    const outfilePath = `${outputFolder}/INTENT-${sourceIntent.name}.json`;
+
+    // Setup new Entity structure, and set some defaults:
+    let newIntent: any;
+
+    // Use the pre-existing file as a template for the new file:
+    // This keeps the Intent as unchanged as possible from what it looks like
+    // when downloaded from Dialogflow:
+    try {
+        newIntent = JSON.parse(fs.readFileSync(outfilePath).toString(),
+            // Use parse() 'reviver' to skip training phrases from disk:    
+            (k, v) => { return (k === "trainingPhrases") ? undefined : v });
+    } catch (e) {
+        newIntent = {};
+    };
+    newIntent.displayName = sourceIntent.name;
+    newIntent.trainingPhrases = [];
+
+    // Detect file read failure OR a file just lacking important details:        
+    if (typeof newIntent.isFallBack === 'undefined') {
+        WARN(`WARNING:Couldn't read Intent config details in file '${outfilePath}'.`);
+        WARN(`       :Output file will likely NOT be importable into Dialogflow.`);
+        WARN(`       :Recommend downloading a working Intent as a skeleton.`);
+    }
+
+    try {
+        for (let sentence of sourceIntent.expandedSentences) {
+            let templatePart = getDialogflowTemplatePart(sentence, sourceIntent);
+            let newPhrase = {
+                parts: [templatePart],
+                name: undefined,   // TODO: Blank GUIDs(Names) might be problematic...
+                type: "TEMPLATE",
+                timesAddedCount: 0
+            };
+            newIntent.trainingPhrases.push(newPhrase);
         }
+        console.log(`Writing to ${outfilePath}`);
+        fs.writeFileSync(outfilePath, JSON.stringify(newIntent, null, 2));
+
+    } catch (err) {
+        ERROR(`ERROR: ${err}\nWriting Dialogflow intent file: ${outfilePath}`);
+        process.exit(1);
     }
 }
 
-function getDialogflowUserSaysItem(sentence: string, sourceIntent: SourceIntent): DialogflowUserSaysItem {
+function writeEntity(sourceEntity, outputFolder) {
+    const outfilePath = `${outputFolder}/ENTITY-${sourceEntity.name}.json`;
+
+    // Setup new Entity structure, and set some defaults:
+    let newEntity = {
+        name: undefined,
+        displayName: sourceEntity.name,
+
+        kind: "KIND_MAP",
+        autoExpansionMode: "AUTO_EXPANSION_MODE_UNSPECIFIED",
+        entities: []
+    };
+
+
+    // Try to get some fields from existing file:
+    // This keeps the Entity as unchanged as possible from what it looks like
+    // when downloaded from Dialogflow:
+    try {
+        let onDiskEntity = JSON.parse(fs.readFileSync(outfilePath).toString());
+        newEntity.name = onDiskEntity.name;
+        newEntity.kind = onDiskEntity.kind;
+        newEntity.autoExpansionMode = onDiskEntity.AUTO_EXPANSION_MODE_UNSPECIFIED;
+    } catch (e) { };
+
+
+    // Now construct the core content of the entity:
+    try {
+        for (let sentence of sourceEntity.expandedSentences) {
+            let entityItem: DialogflowEntityItem = { value: sentence, synonyms: [sentence] };
+            newEntity.entities.push(entityItem);
+        }
+        console.log(`Writing to ${outfilePath}`);
+        fs.writeFileSync(outfilePath, JSON.stringify(newEntity, null, 2));
+    } catch (err) {
+        ERROR(`ERROR: ${err}\nWriting Dialogflow entity file: ${outfilePath}`);
+        process.exit(1);
+    }
+}
+
+function getDialogflowTemplatePart(sentence: string, sourceIntent: SourceIntent): DialogflowUserSaysItem {
+
+    // We create a Dialogflow sentence in TEMPLATE mode, because:
+    // a) It's a simpler, more compact structure
+    // b) It doesn't need sample text for the slot position.
+    // It need to look something like this:
+    // {
+    //     "parts": [
+    //         {
+    //             "text": "I just fed the @Animal:Animal",
+    //             "entityType": "",
+    //             "alias": "",
+    //             "userDefined": false
+    //         }
+    //     ],
+    //     "name": "4d284def-9442-41ab-9db6-61040ec7f994",
+    //     "type": "TEMPLATE",
+    //     "timesAddedCount": 0
+    // },
+
+    let dfText = sentence.replace(/(<[^>]+>)/g, (slotSpec: string) => {
+        let slotName = slotSpec.substr(1, slotSpec.length - 2);
+        let slotType = sourceIntent.getSlot(slotName).type;
+        return `@${slotType}:${slotName}`;
+    });
+
+    let trainingPhrase: DialogflowUserSaysItem = {
+        parts: [{
+            text: dfText,
+            entityType: "",
+            alias: "",
+            userDefined: false
+        }],
+        name: undefined, type: "TEMPLATE", timesAddedCount: 0
+    };
+    return trainingPhrase;
+}
+
+function getDialogflowUserSaysItem_V1(sentence: string, sourceIntent: SourceIntent): DialogflowUserSaysItem_V1 {
 
     // We create a Dialogflow sentence in TEMPLATE mode, because:
     // a) It's a simpler, more compact structure
@@ -112,7 +218,7 @@ function getDialogflowUserSaysItem(sentence: string, sourceIntent: SourceIntent)
     //     "updated": 1520250450
     //   },
 
-    let userSaysItem: DialogflowUserSaysItem = { id: '', data: [], isTemplate: true, count: 0, updated: 1503833609 };
+    let userSaysItem: DialogflowUserSaysItem_V1 = { id: '', data: [], isTemplate: true, count: 0, updated: 1503833609 };
 
     let dfText = sentence.replace(/(<[^>]+>)/g, (slotSpec: string) => {
         let slotName = slotSpec.substr(1, slotSpec.length - 2);
